2dc9ca55f717eca2c11f90ed0cff4718208fd686
Cairo Ramos <cairoramos7@gmail.com>
2026-01-18 00:40:55 -0300

    chore: add ESLint and Prettier configuration
    - Configure ESLint for Node.js with Prettier integration
    - Add Prettier config with single quotes and trailing commas
    - Add .prettierignore for generated files

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    node: true,
+    commonjs: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'no-console': 'off',
+    'prefer-const': 'error',
+    'no-var': 'error',
+    eqeqeq: ['error', 'always'],
+    curly: ['error', 'all'],
+  },
+};
